a1223fadd9659e642d81625e00362cd79b826626
DiegoST001 <diegosancheztafur3@gmail.com>
2024-11-07 14:19:25 -0500

    commit_fi

--- src/main/java/com/tecsup/petclinic/services/VisitServiceImpl.java
+++ src/main/java/com/tecsup/petclinic/services/VisitServiceImpl.java
@@ -1,7 +1,6 @@
 package com.tecsup.petclinic.services;
 
 import com.tecsup.petclinic.entities.Visit;
-import com.tecsup.petclinic.exception.VisitNotFoundException;
 import com.tecsup.petclinic.repositories.VisitRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,17 +24,15 @@ public class VisitServiceImpl implements VisitService {
     }
 
     @Override
-    public void delete(Long id) throws VisitNotFoundException {
-        if (!visitRepository.existsById(id)) {
-            throw new VisitNotFoundException("Visit not found with id " + id);
+    public void delete(Long id) {
+        if (visitRepository.existsById(id)) {
+            visitRepository.deleteById(id);
         }
-        visitRepository.deleteById(id);
     }
 
     @Override
-    public Visit findById(Long id) throws VisitNotFoundException {
-        return visitRepository.findById(id)
-                .orElseThrow(() -> new VisitNotFoundException("Visit not found with id " + id));
+    public Visit findById(Long id) {
+        return visitRepository.findById(id).orElse(null);
     }
 
     @Override
